1918c49bdf951a6466197bf067e5c34ace660599
Claris <clarareyes7116@gmail.com>
2025-08-07 20:41:23 +0000

    outline, graphs, clean up

--- posts/projectpost/data4project.R
+++
@@ -1,6 +0,0 @@
-library(kagglehub)
-
-# Download latest version
-path = kagglehub.dataset_download("alessandrolobello/agri-food-co2-emission-dataset-forecasting-ml")
-
-print("Path to dataset files:", path)
